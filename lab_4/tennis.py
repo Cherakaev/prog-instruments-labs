d630a556eaa5f5682420073a4f74b1f6fc1da3d8
Cherakaev <160835684+Cherakaev@users.noreply.github.com>
2025-12-17 21:22:46 +0400

    extract methods to improve readability in TennisGame1

--- lab_4/tennis.py
+++ lab_4/tennis.py
@@ -14,37 +14,47 @@ class TennisGame1:
             self.p2points += 1
 
     def score(self):
-        result = ""
-        temp_score = 0
         if self.p1points == self.p2points:
-            result = {
-                0: "Love-All",
-                1: "Fifteen-All",
-                2: "Thirty-All",
-            }.get(self.p1points, "Deuce")
-        elif (self.p1points >= 4 or self.p2points >= 4):
-            minusResult = self.p1points - self.p2points
-            if (minusResult == 1):
-                result = "Advantage " + self.player1Name
-            elif (minusResult == -1):
-                result = "Advantage " + self.player2Name
-            elif (minusResult >= 2):
-                result = "Win for " + self.player1Name
-            else:
-                result = "Win for " + self.player2Name
+            return self._get_tie_score()
+        elif self.p1points >= 4 or self.p2points >= 4:
+            return self._get_endgame_score()
         else:
-            for i in range(1, 3):
-                if (i == 1):
-                    temp_score = self.p1points
-                else:
-                    result += "-"
-                    temp_score = self.p2points
-                result += {
-                    0: "Love",
-                    1: "Fifteen",
-                    2: "Thirty",
-                    3: "Forty",
-                }[temp_score]
+            return self._get_running_score()
+
+    def _get_tie_score(self):
+        result = {
+            0: "Love-All",
+            1: "Fifteen-All",
+            2: "Thirty-All",
+        }.get(self.p1points, "Deuce")
+        return result
+
+    def _get_endgame_score(self):
+        minusResult = self.p1points - self.p2points
+        if minusResult == 1:
+            return "Advantage " + self.player1Name
+        elif minusResult == -1:
+            return "Advantage " + self.player2Name
+        elif minusResult >= 2:
+            return "Win for " + self.player1Name
+        else:
+            return "Win for " + self.player2Name
+
+    def _get_running_score(self):
+        result = ""
+        tempScore = 0
+        for i in range(1, 3):
+            if i == 1:
+                tempScore = self.p1points
+            else:
+                result += "-"
+                tempScore = self.p2points
+            result += {
+                0: "Love",
+                1: "Fifteen",
+                2: "Thirty",
+                3: "Forty",
+            }[tempScore]
         return result
 
 
